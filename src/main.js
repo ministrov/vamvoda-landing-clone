1dbc909ea37289e7d80248ec1b58e895fc829b60
Zhila <ministrov2018@gmail.com>
2025-12-26 14:37:15 +0300

    feat: start and end up operating on creating a reviews styling css section

--- src/main.js
+++ src/main.js
@@ -1,4 +1,28 @@
-import { setupCounter } from './js/counter';
-
-// document.body.style.backgroundColor = `red`;
-setupCounter();
+var reviewsSlider = new Swiper('.reviews-slider', {
+  slidesPerView: 4,
+  spaceBetween: 20,
+  loop: true,
+  centerMode: true,
+  navigation: {
+    nextEl: '.navigation__button--next',
+    prevEl: '.navigation__button--prev',
+  },
+  pagination: {
+    el: '.reviews__pagination',
+    clickable: true,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 10,
+    },
+    1231: {
+      slidesPerView: 4,
+      spaceBetween: 20,
+    },
+  },
+});
